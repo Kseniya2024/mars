7b7ea47102d59a7f8cc5b8dadf11747997fbc565
Kseniya2024 <145314959+Kseniya2024@users.noreply.github.com>
2023-09-23 15:09:24 +1000

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,29 @@
+let form = document.querySelector('.form')
+let openForm = document.querySelector('.header__btn')
+let clouseForm = document.querySelector('.form__clouse')
+
+
+openForm.addEventListener('click', function () {
+    form.classList.add('open')
+})
+clouseForm.addEventListener('click', function () {
+    form.classList.remove('open')
+})
+
+let bg = document.querySelector('.main-bg')
+
+$(window).width(function () {
+
+    if ($(window).width() < 768) { /*разрешение, ниже которого анимация должна быть отключена*/
+        document.onmousemove = e => {
+            let x = e.clientX / window.innerWidth;
+
+            bg.style.transform = "translate(-" + x * 50 + "px)"
+        };
+    }
+
+
+})
+
+
 
